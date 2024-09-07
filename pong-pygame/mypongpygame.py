c72b08437c6415d7f7c0076496a4afdd028837f6
Joao Pedro Telles Paes <jptpa.snf24@uea.edu.br>
2024-09-07 15:43:26 -0400

    adicionei o sistema de mudar o ângulo de cada batida dependendo de onde bateu a bola no taco. também fiz a bola aumentar a velocidade lentamente e exponencialmente. não sei se era pra escrever o comentario de commit em inglês mas se for eu faço em inglês nas próximas atividades

--- pong-pygame/mypongpygame.py
+++ pong-pygame/mypongpygame.py
@@ -21,11 +21,14 @@ score_text = score_font.render('00 x 00', True, COLOR_WHITE, COLOR_BLACK)
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (680, 50)
 
-# debug text
-debug_font = pygame.font.Font(r'pong-pygame\assets\PressStart2P.ttf', 20)
+# debug text remove later
+debug_font = pygame.font.Font(r'pong-pygame\assets\PressStart2P.ttf', 10)
 debug_text = debug_font.render('00 x 00', True, COLOR_WHITE, COLOR_BLACK)
 debug_text_rect = debug_text.get_rect()
-debug_text_rect.center = (680, 500)
+debug_text_rect.center = (50, 120)
+debug_text_2 = debug_font.render('debug', True, COLOR_WHITE, COLOR_BLACK)
+debug_text_2_rect = debug_text_2.get_rect()
+debug_text_2_rect.center = (50, 150)
 
 # victory text
 victory_font = pygame.font.Font(r'pong-pygame\assets\PressStart2P.ttf', 100)
@@ -42,17 +45,25 @@ player_1 = pygame.image.load(r"pong-pygame\assets\player.png")
 player_1_y = 300
 player_1_move_up = False
 player_1_move_down = False
+player_1_center = 0
+player_1_top = 0
+player_1_bottom = 0
 
 # player 2 - robot
 player_2 = pygame.image.load(r"pong-pygame\assets\player.png")
 player_2_y = 300
+player_2_move_up = False
+player_2_move_down = False
+player_2_center = 0
+player_2_top = 0
+player_2_bottom = 0
 
 # ball
 ball = pygame.image.load(r"pong-pygame\assets\ball.png")
 ball_x = 640
 ball_y = 360
 ball_dx = 5
-ball_dy = 5
+ball_dy = -5
 ball_relative_y_p1 = 0
 ball_relative_y_p2 = 0
 
@@ -99,20 +110,22 @@ while game_loop:
             ball_dy *= -1
             bounce_sound_effect.play()
 
+        # define coordinates of important positions of each paddle
+        player_1_center = player_1_y + 62
+        player_2_center = player_2_y + 62
+
+        player_1_top = player_1_center + 75
+        player_2_top = player_1_center + 75
+        player_1_bottom = player_1_center + 76
+        player_2_bottom = player_2_center + 76
+
         # ball collision with the player 1 's paddle
-        if ball_x < 100 and ball_dx < 0:
+        if ball_x < 100 and ball_dx < 40:
             if player_1_y < ball_y + 25:
                 if player_1_y + 150 > ball_y:
                     ball_dx *= -1
-                    ball_dx += 1
-                    if (ball_dy < 0 and ball_relative_y_p1 > 0) or (ball_dy > 0 and ball_relative_y_p1 < 0):
-                        ball_dy *= -1 
-                    ball_dy = ball_relative_y_p1 * 10
-
-                    if ball_dy > 50:
-                        ball_dy = 50
-                    if ball_dy < -50:
-                        ball_dy = -50
+                    ball_dx *= 1.05
+                    ball_dy += ball_relative_y_p1 * 4
                     bounce_sound_effect.play()
 
         # ball collision with the player 2 's paddle
@@ -122,12 +135,18 @@ while game_loop:
                     ball_dx *= -1
                     bounce_sound_effect.play()
 
+        # ball maximum y axis speed
+        if ball_dy > 15:
+            ball_dy = 15
+        if ball_dy < -15:
+            ball_dy = -15
+
         # scoring points
         if ball_x < -50:
             ball_x = 640
             ball_y = 360
-            ball_dy *= -1
-            ball_dx *= -1
+            ball_dy = random.randrange(-5, 5)
+            ball_dx = random.choice([5,-5])
             score_2 += 1
             scoring_sound_effect.play()
         elif ball_x > 1320:
@@ -170,6 +189,7 @@ while game_loop:
             player_2_y = 570
 
         # update score hud
+        
         score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
 
         # drawing objects
@@ -177,6 +197,7 @@ while game_loop:
         screen.blit(player_1, (50, player_1_y))
         screen.blit(player_2, (1180, player_2_y))
         screen.blit(score_text, score_text_rect)
+        
     else:
         # drawing victory
         screen.fill(COLOR_BLACK)
@@ -185,13 +206,13 @@ while game_loop:
 
     # update values
     hidden_score += 1
-    if ball_y <= player_1_y + 25:
-        ball_relative_y_p1 = ((player_1_y + 25) - ball_dy) / 175
-    if ball_y > player_1_y - 150:
-        ball_relative_y_p1 = (ball_dy - (player_1_y - 150)) / 175
 
-    debug_text = debug_font.render(str(ball_relative_y_p1), True, COLOR_WHITE, COLOR_BLACK)
-
+    ball_relative_y_p1 = (ball_y - player_1_center)/75
+    # remove later debug info
+    debug_text = debug_font.render(f'{ball_relative_y_p1:.2f} | player 1 center pos: {player_1_center} | ball x speed: {ball_dx:.2f} | ball y speed: {ball_dy:.2f}', True, COLOR_WHITE, COLOR_BLACK)
+    debug_text2 = debug_font.render(f'DEBUG', True, COLOR_WHITE, COLOR_BLACK)
+    screen.blit(debug_text_2, debug_text_rect)
+    screen.blit(debug_text,debug_text_2_rect)
     # update screen
     pygame.display.flip()
     game_clock.tick(60)
